5f91a888cab3dd5e246365019312bcca3011e890
71mM <97466779+71mM@users.noreply.github.com>
2024-09-09 13:09:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from sklearn.model_selection import train_test_split
 
 X, y = load_data()
 print(y.value_counts())
-input()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
